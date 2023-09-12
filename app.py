58a60e40c7b78a773a5ab46b2a606c1f025aa9b6
Häggblom Martin <habmarti@cubbli22-gold.local>
2023-09-12 12:05:56 +0300

    Links to abbrevations, stakeholders, literature, discussions working

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2:///habmarti?host=/home/habmarti/pgsql/sock"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "Welcome to BCI's little helper"
+    return render_template("index.html")
 
 @app.route("/abbrevations")
 def abbrevations():
