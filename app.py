822b50d58e18fb9137e3a6be4745544a4909e1f9
Häggblom Martin <habmarti@cubbli22-gold.local>
2023-09-12 13:00:34 +0300

    Search BCI abbrevation functionality added

--- app.py
+++ app.py
@@ -11,9 +11,28 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
-@app.route("/abbrevations")
+@app.route("/abbrevations", methods=["GET", "POST"])
 def abbrevations():
-    return "WIP BCI abbrevations"
+    abbreviation = None
+    explanation = None
+
+    if request.method == "POST":
+        # Retrieve the abbreviation entered by the user
+        abbreviation = request.form.get("abbreviation")
+
+        # Query the database for the explanation
+        result = db.session.execute(
+            text("SELECT explanation FROM abbrevations WHERE abbrevation = :abbreviation"),
+            {"abbreviation": abbreviation}
+        )
+        explanation = result.fetchone()
+
+    return render_template("abbrevations.html", abbreviation=abbreviation, explanation=explanation)
+
+
+#@app.route("/abbrevations")
+#def abbrevations():
+    #return "WIP BCI abbrevations"
 
 @app.route("/stakeholders")
 def stakeholders():
