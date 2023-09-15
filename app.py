eba3aa7e7145594b7262c5237f0c6f5e2476c283
Häggblom Martin <habmarti@cubbli22-gold.local>
2023-09-15 16:23:40 +0300

    BCI literature search feature working

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask
-from flask import redirect, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-#setting environment variable (Osa 2: Ympäristömuuttujat)
 from os import getenv
+from sqlalchemy import or_
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-
-#setting environment variable (Osa 2: Ympäristömuuttujat)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
@@ -34,18 +31,43 @@ def abbrevations():
 
     return render_template("abbrevations.html", abbreviation=abbreviation, explanation=explanation)
 
-
-#@app.route("/abbrevations")
-#def abbrevations():
-    #return "WIP BCI abbrevations"
-
 @app.route("/stakeholders")
 def stakeholders():
     return "WIP BCI stakeholders"
 
-@app.route("/literature")
+@app.route("/literature", methods=["GET", "POST"])
 def literature():
-    return "WIP BCI literature"
+    title = request.form.get("title")
+    author = request.form.get("author")
+    keywords = request.form.get("keywords")
+    rating = request.form.get("rating")
+    availability = request.form.get("availability")
+
+    # Build the SQL query dynamically based on the provided fields
+    query = "SELECT * FROM literature WHERE 1=1"
+    params = {}
+
+    if title:
+        query += " AND title = :title"
+        params["title"] = title
+    if author:
+        query += " AND author = :author"
+        params["author"] = author
+    if keywords:
+        query += " AND keywords = :keywords"
+        params["keywords"] = keywords
+    if rating:
+        query += " AND rating = :rating"
+        params["rating"] = rating
+    if availability:
+        query += " AND availability = :availability"
+        params["availability"] = availability
+
+    # Execute the SQL query
+    result = db.session.execute(text(query), params)
+    results = result.fetchall()
+
+    return render_template("literature.html", title=title, author=author, keywords=keywords, rating=rating, availability=availability, results=results)
 
 @app.route("/discussions")
 def discussions():
