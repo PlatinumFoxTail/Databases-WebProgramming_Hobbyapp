03564f9cb0607fb30be98ffd9581925c53ddd8a5
Häggblom Martin <habmarti@cubbli22-gold.local>
2023-09-17 08:04:59 +0300

    Literature addition working, but not together with Literature search

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
@@ -7,6 +7,7 @@ from sqlalchemy import or_
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "your_secret_key"  # Flask's session management requires a secret key to be set for security reasons. Needed at least for adding literature in literature page.
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -37,37 +38,57 @@ def stakeholders():
 
 @app.route("/literature", methods=["GET", "POST"])
 def literature():
-    title = request.form.get("title")
-    author = request.form.get("author")
-    keywords = request.form.get("keywords")
-    rating = request.form.get("rating")
-    availability = request.form.get("availability")
+    if request.method == "POST":
+        # Handle the form submission for adding a new literature item
+        title = request.form.get("title")
+        author = request.form.get("author")
+        keywords = request.form.get("keywords")
+        rating = request.form.get("rating")
+        availability = request.form.get("availability")
 
-    # Build the SQL query dynamically based on the provided fields
-    query = "SELECT * FROM literature WHERE 1=1"
-    params = {}
+        # Insert the new literature item into the database
+        db.session.execute(
+            text("INSERT INTO literature (title, author, keywords, rating, availability) VALUES (:title, :author, :keywords, :rating, :availability)"),
+            {"title": title, "author": author, "keywords": keywords, "rating": rating, "availability": availability}
+        )
+        db.session.commit()
+        flash("Literature item added successfully", "success")
 
-    if title:
-        query += " AND title = :title"
-        params["title"] = title
-    if author:
-        query += " AND author = :author"
-        params["author"] = author
-    if keywords:
-        query += " AND keywords = :keywords"
-        params["keywords"] = keywords
-    if rating:
-        query += " AND rating = :rating"
-        params["rating"] = rating
-    if availability:
-        query += " AND availability = :availability"
-        params["availability"] = availability
+        return redirect(url_for("literature"))
+    return render_template("literature.html")
 
-    # Execute the SQL query
-    result = db.session.execute(text(query), params)
-    results = result.fetchall()
 
-    return render_template("literature.html", title=title, author=author, keywords=keywords, rating=rating, availability=availability, results=results)
+    #title = request.form.get("title")
+    #author = request.form.get("author")
+    #keywords = request.form.get("keywords")
+    #rating = request.form.get("rating")
+    #availability = request.form.get("availability")
+
+    ## Build the SQL query dynamically based on the provided fields
+    #query = "SELECT * FROM literature WHERE 1=1"
+    #params = {}
+
+    #if title:
+        #query += " AND title = :title"
+        #params["title"] = title
+    #if author:
+        #query += " AND author = :author"
+        #params["author"] = author
+    #if keywords:
+        #query += " AND keywords = :keywords"
+        #params["keywords"] = keywords
+    #if rating:
+        #query += " AND rating = :rating"
+        #params["rating"] = rating
+    #if availability:
+        #query += " AND availability = :availability"
+        #params["availability"] = availability
+
+    ## Execute the SQL query
+    #result = db.session.execute(text(query), params)
+    #results = result.fetchall()
+
+    #return render_template("literature.html", title=title, author=author, keywords=keywords, rating=rating, availability=availability, results=results)
 
 @app.route("/discussions")
 def discussions():
