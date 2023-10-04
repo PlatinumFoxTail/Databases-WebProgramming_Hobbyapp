26d112a098ebd63e9b76bef191e8930a4cb29ba3
Martin Häggblom <140994803+PlatinumFoxTail@users.noreply.github.com>
2023-10-04 15:15:42 +0300

    literature table updated in schema.sql
    Changed type of column from mostly "characters varying" to "text"

--- schema.sql
+++ schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (id integer, username text, password text, role integer);
 
 CREATE TABLE abbrevations (id integer, abbrevation text, explanation text);
 
-CREATE TABLE literature (id integer, title character varying, author character varying, keywords ARRAY, rating integer, availability character varying);
+CREATE TABLE literature (id integer, title text, author text, keywords text, rating integer, availability text);
 
 CREATE TABLE stakeholders (id integer, name text, type text, description text, contact text);
