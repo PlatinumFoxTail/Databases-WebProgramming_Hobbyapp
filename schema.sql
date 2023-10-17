0e6b0fccb4c1768678a418b4a0ac3c45dad37429
Martin Häggblom <140994803+PlatinumFoxTail@users.noreply.github.com>
2023-10-17 09:20:04 +0300

    ; missing from abbrevations tables

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, role INTEGER);
 
-CREATE TABLE abbrevations (id SERIAL PRIMARY KEY, abbrevation TEXT, explanation TEXT)
+CREATE TABLE abbrevations (id SERIAL PRIMARY KEY, abbrevation TEXT, explanation TEXT);
 
 CREATE TABLE literature (id SERIAL PRIMARY KEY, title TEXT, author TEXT, keywords TEXT, rating INTEGER, availability TEXT);
 
